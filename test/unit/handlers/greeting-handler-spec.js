60a1629b67668f1394212e8c7a62d8321383d56f
vamship <vamshi.ponnapalli@gmail.com>
2016-08-25 13:45:46 -0400

    Using utility from wysknd-test to generate error message patterns

--- test/unit/handlers/greeting-handler-spec.js
+++ test/unit/handlers/greeting-handler-spec.js
@@ -14,6 +14,8 @@ const _rewire = require('rewire');
 const _testHelper = require('wysknd-test');
 const LambdaWrapper = _testHelper.AwsLambdaWrapper;
 const _testValueProvider = _testHelper.testValueProvider;
+const _testUtils = _testHelper.utils;
+
 let _greetingHandler = null;
 
 describe('[greeting handler]', () => {
@@ -21,10 +23,6 @@ describe('[greeting handler]', () => {
     const DEFAULT_FIRST_NAME = 'John';
     const DEFAULT_LAST_NAME = 'Doe';
     const DEFAULT_MIDDLE_NAME = 'Samuel';
-
-    function _generateErrorRegex(property) {
-        return new RegExp(`\\[SchemaError\\] Schema validation failed. Details: \\[.*${property}.*\\]`);
-    }
 
     function _createWrapper(event, alias, config) {
         event = event || {};
@@ -49,7 +47,7 @@ describe('[greeting handler]', () => {
     describe('[input validation]', () => {
 
         it('should fail execution if a valid language is not specified', () => {
-            const error = _generateErrorRegex('language');
+            const error = _testUtils.generateSchemaErrorPattern('language');
 
             _testValueProvider.allButString('').forEach((language) => {
                 const wrapper = new LambdaWrapper(_greetingHandler, {
@@ -60,7 +58,7 @@ describe('[greeting handler]', () => {
         });
 
         it('should fail execution if the event does not define a valid user object', () => {
-            const error = _generateErrorRegex('user');
+            const error = _testUtils.generateSchemaErrorPattern('user');
             _testValueProvider.allButObject().forEach((user) => {
                 const wrapper = new LambdaWrapper(_greetingHandler, {
                     language: DEFAULT_LANGUAGE,
@@ -71,7 +69,7 @@ describe('[greeting handler]', () => {
         });
 
         it('should fail execution if the user object does not define a valid firstName property', () => {
-            const error = _generateErrorRegex('firstName');
+            const error = _testUtils.generateSchemaErrorPattern('firstName');
             _testValueProvider.allButString('').forEach((firstName) => {
                 const wrapper = new LambdaWrapper(_greetingHandler, {
                     language: DEFAULT_LANGUAGE,
@@ -84,7 +82,7 @@ describe('[greeting handler]', () => {
         });
 
         it('should fail execution if the user object does not define a valid lastName property', () => {
-            const error = _generateErrorRegex('lastName');
+            const error = _testUtils.generateSchemaErrorPattern('lastName');
             _testValueProvider.allButString('').forEach((lastName) => {
                 const wrapper = new LambdaWrapper(_greetingHandler, {
                     language: DEFAULT_LANGUAGE,
@@ -98,7 +96,7 @@ describe('[greeting handler]', () => {
         });
 
         it('should fail execution if the user object defines a middleName property of the incorrect type', () => {
-            const error = _generateErrorRegex('middleName');
+            const error = _testUtils.generateSchemaErrorPattern('middleName');
             _testValueProvider.allButSelected('null', 'undefined', 'string').forEach((middleName) => {
                 const wrapper = new LambdaWrapper(_greetingHandler, {
                     language: DEFAULT_LANGUAGE,
